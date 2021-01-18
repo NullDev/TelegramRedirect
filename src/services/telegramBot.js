2d8580d45ecc85ff28a84be3128beef3bd1b471d
NullDev (Chris) <shadow@nulldev.org>
2021-01-18 17:20:11 +0100

    Rework

--- src/services/telegramBot.js
+++ src/services/telegramBot.js
@@ -15,13 +15,19 @@ let bot = new TelegramBot(config.telegram.bot_token, { polling: true });
 
 let validUrl = url => !!(new RegExp("^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$", "i")).test(url);
 
+let URL = config.server.default_url;
+
 module.exports = async function(app){
-    bot.on("message", msg => {
+    app.get("/", (req, res) => res.status(301).redirect(URL));
+
+    log.info("Bot listening...");
+    bot.on("message", async msg => {
         if (!msg.text || !validUrl(msg.text)){
             log.warn(`Invalid URL (${msg.text}) prodided by User ${msg.from.username} (${msg.from.id})`);
             return bot.sendMessage(msg.chat.id, "This seems like an Invalid URL =(");
         }
         log.info(`Set new URL to: ${msg.text}`);
-        return app.get("/", (req, res) => res.status(301).redirect(msg.text));
+        URL = msg.text;
+        return bot.sendMessage(msg.chat.id, "New link has been set! =)");
     });
 };
